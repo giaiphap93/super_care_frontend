d5e14ec20039f922145dc6de5258cb99a407577e
Thao Huynh <thao.huynh@navigosgroup.com>
2023-07-18 18:35:50 +0700

    update

--- src/PublicLayout.vue
+++ src/PublicLayout.vue
@@ -1,8 +1,38 @@
 <template>
-  <router-view />
+  <div class="form__container">
+    <a-row>
+      <a-col :span="8" style="background-image: url(https://picsum.photos/1000);">
+
+      </a-col>
+      <a-col :span="16">
+        <div class="form__component">
+          <router-view />
+        </div>
+      </a-col>
+    </a-row>
+
+  </div>
 </template>
 <script>
-  export default {
-    name: 'PublicLayout',
-  };
+export default {
+  name: 'PublicLayout',
+};
 </script>
+<style scoped>
+.form__container {
+  border: 1px solid #ccc;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 50%;
+}
+
+.form__component {
+  padding: 2rem;
+}
+
+body {
+  background: grey !important;
+}
+</style>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,59 +1,36 @@
 <template>
-  <a-row>
-    <a-col :span="12" >
-      <div class="t-flex t-items-center t-justify-center t-w-full">
-        <img
-          class="t-h-10 t-w-auto"
-          src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png"
-        />
-      </div>
-      <h1 class="t-text-center">Login to account</h1>
-      <p class="t-text-center">New to [Abcxyz]? <router-link to="/users/register">Create your business</router-link></p>
-      <div class="t-flex t-items-center t-justify-center t-w-full">
-        <a-form
-          :model="loginForm"
-          name="basic"
-          :label-col="{ span: 8 }"
-          :wrapper-col="{ span: 16 }"
-          autocomplete="off"
-          @finish="onFinish"
-          @finishFailed="onFinishFailed"
-          class="login__form"
-        >
-          <a-form-item
-            label="Username"
-            name="username"
-            :rules="[
-              { required: true, message: 'Please input your username!' },
-            ]"
-          >
-            <a-input v-model:value="loginForm.username" />
-          </a-form-item>
+  <div>
+    <div class="t-flex t-items-center t-justify-center t-w-full">
+      <img class="t-h-10 t-w-auto"
+        src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png" />
+    </div>
+    <h1 class="t-text-center">Login to account</h1>
+    <p class="t-text-center">New to [Abcxyz]? <router-link to="/users/register">Create your business</router-link></p>
+    <div class="t-flex t-items-center t-justify-center t-w-full">
+      <a-form :model="loginForm" name="basic" :label-col="{ span: 8 }" :wrapper-col="{ span: 16 }" autocomplete="off"
+        @finish="onFinish" @finishFailed="onFinishFailed" class="login__form">
+        <a-form-item label="Username" name="username" :rules="[
+          { required: true, message: 'Please input your username!' },
+        ]">
+          <a-input v-model:value="loginForm.username" />
+        </a-form-item>
 
-          <a-form-item
-            label="Password"
-            name="password"
-            :rules="[
-              { required: true, message: 'Please input your password!' },
-            ]"
-          >
-            <a-input-password v-model:value="loginForm.password" />
-          </a-form-item>
+        <a-form-item label="Password" name="password" :rules="[
+          { required: true, message: 'Please input your password!' },
+        ]">
+          <a-input-password v-model:value="loginForm.password" />
+        </a-form-item>
 
-          <a-form-item name="remember" :wrapper-col="{ offset: 8, span: 16 }">
-            <a-checkbox v-model:checked="loginForm.remember"
-              >Remember me</a-checkbox
-            >
-          </a-form-item>
+        <a-form-item name="remember" :wrapper-col="{ offset: 8, span: 16 }">
+          <a-checkbox v-model:checked="loginForm.remember">Remember me</a-checkbox>
+        </a-form-item>
 
-          <a-form-item :wrapper-col="{ offset: 8, span: 16 }">
-            <a-button type="primary" html-type="submit">Login</a-button>
-          </a-form-item>
-        </a-form>
-      </div>
-    </a-col>
-    <a-col :span="12">12</a-col>
-  </a-row>
+        <a-form-item :wrapper-col="{ offset: 8, span: 16 }">
+          <a-button type="primary" html-type="submit">Login</a-button>
+        </a-form-item>
+      </a-form>
+    </div>
+  </div>
 </template>
 <script>
 export default {
